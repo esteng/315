aab3139c1699e59e1df4d72f963708649dfa07fe
esteng <elias.stengel@gmail.com>
2016-11-06 16:08:09 -0500

    more plots

--- A2/results/make_comparisons.py
+++ A2/results/make_comparisons.py
@@ -3,14 +3,15 @@ import os
 
 
 all_my_lines = []
-for folder in os.walk("/Users/Elias/315/A2/results/my_learnability"):
-    for f in folder[2]:
-        if f == "outcomes" and int(folder[0][-2:])%10 == 0:
-            with open(os.path.join(folder[0].strip(), f.strip())) as f1:
-                lines = [x.strip() for x in f1.readlines()]
-                all_my_lines.append(lines)
+for i in range(10, 101, 10):
+    with open("/Users/Elias/315/A2/results/my_learnability/my_learnability={}/outcomes".format(i)) as f1:
+        lines = [x.strip().replace("quit","0").replace("win","1") for x in f1.readlines()]
+        all_my_lines.append(lines)
+
+print(all_my_lines)
 f2 = open("my_win_freq.csv", "w")
 f2cw = csv.writer(f2)
+f2cw.writerow([x for x in range(10, 101, 10)])
 while len(all_my_lines[0]) > 0:
     towrite = []
     for s in all_my_lines:
@@ -19,13 +20,17 @@ while len(all_my_lines[0]) > 0:
 
 
 all_orig_lines = []
-for folder in os.walk("/Users/Elias/315/A2/results/original_learnability"):
-    for f in folder[2]:
-        if f == "outcomes" and int(folder[0][-2:])%10 == 0:
-            with open(os.path.join(folder[0].strip(), f.strip())) as f1:
-                lines = [x.strip() for x in f1.readlines()]
-                all_orig_lines.append(lines)
+for i in range(10, 101, 10):
+    with open("/Users/Elias/315/A2/results/original_learnability/original_learnability={}/outcomes".format(i)) as f1:
+        lines = [x.strip().replace("quit","0").replace("win","1").replace("stop","0") for x in f1.readlines()]
+        all_orig_lines.append(lines)
 
 
 f3 = open("orig_win_freq.csv", "w")
 f3cw = csv.writer(f3)
+f3cw.writerow([x for x in range(10, 101, 10)])
+while len(all_orig_lines[0]) > 0:
+    towrite = []
+    for s in all_orig_lines:
+        towrite.append(s.pop(0))
+    f3cw.writerow(towrite)
